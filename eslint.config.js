25743db6a1861e394ea71ac5189242b3b517589f
Uniquebeing <beingunique049@gmail.com>
2026-04-21 20:21:12 +0100

    Update ESLint configuration with new imports

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,10 @@
+
 import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
+
 
 export default tseslint.config(
   { ignores: ["dist"] },
